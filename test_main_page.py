670099b30321313ba7701e1033fb8fa212ac21b0
WoodyFullStack <88949385+WoodyFullStack@users.noreply.github.com>
2022-11-21 21:24:58 +0400

    Now checking that login page is opened

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 
@@ -7,7 +8,6 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page.should_be_login_link()
+    logpage = LoginPage(browser, browser.current_url)
+    logpage.should_be_login_page()
